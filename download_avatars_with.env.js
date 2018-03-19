4a05ac80d45790c47dc3742cfafde9c8e9cc2db0
Nucleartom <jacob.maarse@gmail.com>
2018-03-19 20:19:28 +0000

    small adjustment to logic for error codes

--- download_avatars_with.env.js
+++ download_avatars_with.env.js
@@ -27,7 +27,7 @@ else if (repoOwner && repoName) {
   getRepoContributors(repoOwner, repoName, function(err, result) {
     arrayOfObj = JSON.parse(result);
     for (var i = 0; i <arrayOfObj.length; i++) {
-      //console.log(arrayOfObj);
+      // console.log(arrayOfObj);
       downloadImageByURL(arrayOfObj[i].avatar_url, `avatars/${arrayOfObj[i].login}.jpg`);
       console.log('Download:', i+1," of ", arrayOfObj.length, " complete.");
     }
@@ -51,7 +51,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
   request(options, function(err, res, body) {
-    if (err !== 401 || or 403){
+    if (err !== 401 && err !== 403){
       cb(err, body);
     } else {
       console.log ('repo doesnt exist or authetication failed');
