104959e99145f1b2babc7d731ce1e2530cb0f0d4
Nucleartom <jacob.maarse@gmail.com>
2018-03-19 17:54:20 +0000

    final syntax update/clean up before submission

--- download_avatars.js
+++ download_avatars.js
@@ -10,15 +10,13 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 // Function call
 
 if (repoOwner && repoName) {
-getRepoContributors(repoOwner, repoName, function(err, result) {
-  console.log("Errors:", err);
-  arrayOfObj = JSON.parse(result);
-
-  for (var i = 0; i <arrayOfObj.length; i++) {
-    downloadImageByURL(arrayOfObj[i].avatar_url, `avatars/${arrayOfObj[i].login}.jpg`);
-    console.log('Download:', i+1," of ", arrayOfObj.length, " complete.");
-  };
-});
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+    arrayOfObj = JSON.parse(result);
+    for (var i = 0; i <arrayOfObj.length; i++) {
+      downloadImageByURL(arrayOfObj[i].avatar_url, `avatars/${arrayOfObj[i].login}.jpg`);
+      console.log('Download:', i+1," of ", arrayOfObj.length, " complete.");
+    }
+  });
 }
 else {
   console.log('repoOwner and repoName must be specified');
@@ -27,18 +25,18 @@ else {
 // FUNCTIONS
 
 function downloadImageByURL(url, filePath) {
-request.get(url)
-       .pipe(fs.createWriteStream(filePath));
+  request.get(url)
+  .pipe(fs.createWriteStream(filePath));
 }
+
 function getRepoContributors(repoOwner, repoName, cb) {
-   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
+ var options = {
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': secrets.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, res, body) {
     cb(err, body);
   });
