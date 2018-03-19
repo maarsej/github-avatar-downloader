84c4b3582b398da01401020e73204577364d73ec
Nucleartom <jacob.maarse@gmail.com>
2018-03-19 17:41:22 +0000

    Function now loops through and saves all avatar images in folder called avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -25,6 +26,18 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log(arrayOfObj.length);
   //console.log(arrayOfObj[1].avatar_url);
   for (var i = 0; i <arrayOfObj.length; i++) {
-    console.log(arrayOfObj[i].avatar_url);
+    downloadImageByURL(arrayOfObj[i].avatar_url, `avatars/${arrayOfObj[i].login}.jpg`);
+    console.log('Download:', i+1," of ", arrayOfObj.length, " complete.");
   };
 });
+
+function downloadImageByURL(url, filePath) {
+request.get(url)
+       .pipe(fs.createWriteStream(filePath));
+}
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+// request.get(arrayOfObj[i].avatar_url)
+//        .pipe(fs.createWriteStream(`./avatars/${arrayOfObj[i].login}.jpg`));
+
