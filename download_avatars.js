4bce0001ba7f7ccfa16dc22caf06b82c7c5dee5f
Nucleartom <jacob.maarse@gmail.com>
2018-03-19 17:49:12 +0000

    program now takes repoowner and repo name as input on command line, will not run if not given

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,35 @@
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+
 var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Function call
 
+if (repoOwner && repoName) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
+  console.log("Errors:", err);
+  arrayOfObj = JSON.parse(result);
+
+  for (var i = 0; i <arrayOfObj.length; i++) {
+    downloadImageByURL(arrayOfObj[i].avatar_url, `avatars/${arrayOfObj[i].login}.jpg`);
+    console.log('Download:', i+1," of ", arrayOfObj.length, " complete.");
+  };
+});
+}
+else {
+  console.log('repoOwner and repoName must be specified');
+}
+
+// FUNCTIONS
+
+function downloadImageByURL(url, filePath) {
+request.get(url)
+       .pipe(fs.createWriteStream(filePath));
+}
 function getRepoContributors(repoOwner, repoName, cb) {
    var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -18,26 +43,3 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, body);
   });
 }
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
-  arrayOfObj = JSON.parse(result);
-  console.log(arrayOfObj.length);
-  //console.log(arrayOfObj[1].avatar_url);
-  for (var i = 0; i <arrayOfObj.length; i++) {
-    downloadImageByURL(arrayOfObj[i].avatar_url, `avatars/${arrayOfObj[i].login}.jpg`);
-    console.log('Download:', i+1," of ", arrayOfObj.length, " complete.");
-  };
-});
-
-function downloadImageByURL(url, filePath) {
-request.get(url)
-       .pipe(fs.createWriteStream(filePath));
-}
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
-
-// request.get(arrayOfObj[i].avatar_url)
-//        .pipe(fs.createWriteStream(`./avatars/${arrayOfObj[i].login}.jpg`));
-
